602aa2b7cff8e0fe24cb83bcf99e94f2b6ac7442
Alexis <72978378+Alexis-CAPON@users.noreply.github.com>
2025-09-25 16:12:32 -0400

    initialization

---
+++ base-game/src/core/game/TerrainMapLoader.ts
@@ -0,0 +1,75 @@
+import { GameMapType } from "./Game";
+import { GameMap, GameMapImpl } from "./GameMap";
+import { GameMapLoader } from "./GameMapLoader";
+
+export type TerrainMapData = {
+  manifest: MapManifest;
+  gameMap: GameMap;
+  miniGameMap: GameMap;
+};
+
+const loadedMaps = new Map<GameMapType, TerrainMapData>();
+
+export interface MapMetadata {
+  width: number;
+  height: number;
+  num_land_tiles: number;
+}
+
+export interface MapManifest {
+  name: string;
+  map: MapMetadata;
+  mini_map: MapMetadata;
+  nations: Nation[];
+}
+
+export interface Nation {
+  coordinates: [number, number];
+  flag: string;
+  name: string;
+  strength: number;
+}
+
+export async function loadTerrainMap(
+  map: GameMapType,
+  terrainMapFileLoader: GameMapLoader,
+): Promise<TerrainMapData> {
+  const cached = loadedMaps.get(map);
+  if (cached !== undefined) return cached;
+  const mapFiles = terrainMapFileLoader.getMapData(map);
+
+  const manifest = await mapFiles.manifest();
+  const gameMap = await genTerrainFromBin(
+    manifest.map,
+    await mapFiles.mapBin(),
+  );
+  const miniGameMap = await genTerrainFromBin(
+    manifest.mini_map,
+    await mapFiles.miniMapBin(),
+  );
+  const result = {
+    manifest: await mapFiles.manifest(),
+    gameMap: gameMap,
+    miniGameMap: miniGameMap,
+  };
+  loadedMaps.set(map, result);
+  return result;
+}
+
+export async function genTerrainFromBin(
+  mapData: MapMetadata,
+  data: Uint8Array,
+): Promise<GameMap> {
+  if (data.length !== mapData.width * mapData.height) {
+    throw new Error(
+      `Invalid data: buffer size ${data.length} incorrect for ${mapData.width}x${mapData.height} terrain plus 4 bytes for dimensions.`,
+    );
+  }
+
+  return new GameMapImpl(
+    mapData.width,
+    mapData.height,
+    data,
+    mapData.num_land_tiles,
+  );
+}
